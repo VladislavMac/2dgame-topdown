b4812ec8a2a5d92fc063a24c36720ef9dd5b1bc0
VladislavMac <mvvminsk@gmail.com>
2024-09-01 14:39:05 +0300

    Upload Build Game

--- Assets/Script/Bullet/BaseBullet.cs
+++ Assets/Script/Bullet/BaseBullet.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 using UnityEngine.XR;
 using UnityEditor;
 
